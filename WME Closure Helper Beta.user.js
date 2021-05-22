72ab4ad3239cf8d896c57f36b9df45d97d3a48dc
jm6087 <jm6087waze@gmail.com>
2021-05-22 12:09:40 -0500

    Update WME Closure Helper Beta.user.js
    Co-authored-by: SkulllzzCode <mr.skulllz@gmail.com>

--- WME Closure Helper Beta.user.js
+++ WME Closure Helper Beta.user.js
@@ -1136,7 +1136,7 @@ function addClosureLengthValue() {
             '<span id="wmech_lEB2h" class="wmech_closureButton wmech_lengthExtenderButton" style="background-color: #c9ffba;">+2h</span>',
             '<span id="wmech_lEB1d" class="wmech_closureButton wmech_lengthExtenderButton" style="background-color: #bafff7;">+1d</span>',
             '<span id="wmech_lEB1w" class="wmech_closureButton wmech_lengthExtenderButton" style="background-color: #bdbaff;">+1w</span>',
-            '<span id="wmech_lEB." class="wmech_closureButton wmech_lengthExtenderButton" style="background-color: #ffbaf9;">+1mo</span>',
+            '<span id="wmech_lEB1mo" class="wmech_closureButton wmech_lengthExtenderButton" style="background-color: #ffbaf9;">+1mo</span>',
             '<span id="wmech_lEBcustomMin" class="wmech_closureButton wmech_lengthExtenderButton" style="background-color: #ffffff;">custom</span>',
         ].join("\n");
         $("#wmech_closurelengthval").after("<div id='wmech_timeExtenderDiv'></div>");
