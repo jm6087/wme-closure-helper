ca6525eea76f8b1f09e537459bdc04bc00fdfaac
jm6087 <jm6087waze@gmail.com>
2023-09-26 18:42:06 -0500

    Update WME Closure Helper Beta.user.js

--- WME Closure Helper Beta.user.js
+++ WME Closure Helper Beta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Closure Helper - Beta
 // @namespace    https://greasyfork.org/en/users/673666-fourloop
-// @version      ß 2023.09.26.05
+// @version      ß 2023.09.27.01
 // @description  A script to help out with WME closure efforts! :D
 // @author       fourLoop & maintained by jm6087 fuji2086
 // @match        https://beta.waze.com/*editor*
